022e3bc5a9dfcdbf47227a455f7ef1c2c1a6fcc1
Diego Silva <diegocaetano444@outlook.com>
2022-03-16 11:46:41 -0300

    update RommController

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -2,16 +2,21 @@ const Database = require("../db/config")
 
 module.exports = {
   async create(req, res) {
-
     const db = await Database()
     const pass = req.body.password
     let roomId
 
+    // Gera o numero da sala
     for(var i=0; i<6; i++) {
       i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
       roomId += Math.floor(Math.random() * 10).toString()
     }
 
+    // verifica se  esse  numero ja existe no banco
+    db.run(`SELECT id FROM rooms`)
+
+
+    // Inseri a sala no banco
     await db.run(`INSERT INTO rooms (
       id,
       pass
